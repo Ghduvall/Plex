37387818144b449b85caa2f0d81d88a99658e9b9
Grant Duvall <grant@mark4tech.com>
2022-01-02 00:05:02 -0600

    Fixed finding already uploaded tv shows

--- find_current_tvshows.sh
+++ find_current_tvshows.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
+upshows=$(ls)
+#echo $upshows
 
+echo -e "\n\n HELLO THIS IS \n FINDING ALREADY\n UPLOADED TVSHOWS\n\n"
 
-
-# GREP needs to be changed to a variable that scans current Plex/TV_Shows of uploading computer
-tvlist1=$(ssh "$uploaduser"@"$ipaddress" "ls -l "${disk1}/tv_shows/" | grep "70"")
-tvstr1=$(echo "$tvlist1" | awk '{print $9,$10,$11,$12}')
-echo $tvstr1
+tvlist1=$(ssh "$uploaduser"@"$ipaddress" "ls -l "${disk1}/tv_shows/" | grep $upshows")
+tvstr1=$(echo "$tvlist1" | awk '{print $9,$10,$11,$12,$13}')
 #set -- $tvstr1
 
 # In case the string between * * needs to be set to the same var as grep to search for tvshow being uploaded
 # Using case because if does not allow searcing for substring 
 case "$tvstr1" in
-    *7* ) echo "Found the show on ${disk1}";;
-    *Show* ) echo "do something # 2";;
+    *$upshows* ) echo "Found the show on ${disk1}";;
     * ) echo "Did not find the show on ${disk1}";;
 esac
 
-
-
-tvlist2=$(ssh "$uploaduser"@"$ipaddress" "ls -l "${disk2}/tv_shows/"")
-tvstr2=$(echo "$tvlist2" | awk '{print $9,$10,$11,$12}')
-echo $tvstr2
-
+tvlist2=$(ssh "$uploaduser"@"$ipaddress" "ls -l "${disk2}/tv_shows/" | grep $upshows")
+tvstr2=$(echo "$tvlist2" | awk '{print $9,$10,$11,$12,$13}')
 # Using case because if does not allow searcing for substring 
 case "$tvstr2" in
-    *Rick* ) echo "Found the show on ${disk2}";;
-    *Show* ) echo "do something # 2";;
+    *$upshows*) echo "Found the show on ${disk2}";;
     * ) echo "Did not find the show on ${disk2}";;
 esac
 
+tvlist3=$(ssh "$uploaduser"@"$ipaddress" "ls -l "${disk3}/tv_shows/" | grep $upshows")
+tvstr3=$(echo "$tvlist3" | awk '{print $9,$10,$11,$12,$13}')
+# Using case because if does not allow searcing for substring 
+case "$tvstr3" in
+    *$upshows*) echo "Found the show on ${disk3}";;
+    * ) echo "Did not find the show on ${disk3}";;
+esac
 
+tvlist4=$(ssh "$uploaduser"@"$ipaddress" "ls -l "${disk4}/tv_shows/" | grep $upshows")
+tvstr4=$(echo "$tvlist4" | awk '{print $9,$10,$11,$12,$13}')
+# Using case because if does not allow searcing for substring 
+case "$tvstr4" in
+    *$upshows*) echo "Found the show on ${disk4}";;
+    * ) echo "Did not find the show on ${disk4}";;
+esac
 
-echo -e "\n\n HELLO THIS IS \nFINDING ALREADY\n UPLOADED TVSHOWS\n\n"
+tvlist5=$(ssh "$uploaduser"@"$ipaddress" "ls -l "${disk5}/tv_shows/" | grep $upshows")
+tvstr5=$(echo "$tvlist5" | awk '{print $9,$10,$11,$12,$13}')
+# Using case because if does not allow searcing for substring 
+case "$tvstr5" in
+    *$upshows*) echo "Found the show on ${disk5}";;
+    * ) echo "Did not find the show on ${disk5}";;
+esac
+
 
 #ssh "$uploaduser"@"$ipaddress" ls -l "${disk1}/tv_shows/"
 #ssh "$uploaduser"@"$ipaddress" ls -l "${disk2}/tv_shows/"
 #ssh "$uploaduser"@"$ipaddress" ls -l "${disk3}/tv_shows/"
 #ssh "$uploaduser"@"$ipaddress" ls -l "${disk4}/tv_shows/"
-
-
