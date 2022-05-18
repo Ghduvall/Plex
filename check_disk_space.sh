cce1999da40a432b9b290d9975e3f70c0e87b3f5
Grant Duvall <grant@mark4tech.com>
2022-05-17 22:14:06 -0500

    update

--- check_disk_space.sh
+++ check_disk_space.sh
@@ -3,6 +3,8 @@
 #grep 'pattern1\|pattern2' is how you combine two diffrent searches in one grep
 echo -e "\n________________________________________________________________________________________________"
 echo -e "DISK                    USED  FREE"
-ssh "$uploaduser"@"$ipaddress" df -lh | grep "$grepsearchpattern"
+x=$(ssh "$uploaduser"@"$ipaddress" df -lh | grep "$grepsearchpattern")
+echo -e "$(df -lh | grep Avail)"
+echo -e $x
 echo -e "________________________________________________________________________________________________"
 
