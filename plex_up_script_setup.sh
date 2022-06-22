e43d0505f8043e9c4343ad50004dce43e6ae703b
Grant Duvall <grant@mark4tech.com>
2022-06-21 21:50:25 -0500

    Fixed linux creation of directories

--- plex_up_script_setup.sh
+++ plex_up_script_setup.sh
@@ -2,14 +2,14 @@
 
 # This script is for setting up the required directories for using the up_script.sh
 
-checkpathplex=/Users/$USER/Movies/Plex
-checkpathdownloaded=/Users/$USER/Movies/Plex/Downloaded
-checkpathlists=/Users/$USER/Movies/Plex/Lists
-checkpathmovies=/Users/$USER/Movies/Plex/Movies
-checkpathmusic=/Users/$USER/Movies/Plex/Music
-checkpathtv_shows=/Users/$USER/Movies/Plex/Tv_Shows
-checkpathuploaded=/Users/$USER/Movies/Plex/Uploaded
-checkpathworkout=/Users/$USER/Movies/Plex/Workout
+checkpathplex=~/Movies/Plex/
+checkpathdownloaded=~/Movies/Plex/Downloaded/
+checkpathlists=~/Movies/Plex/Lists/
+checkpathmovies=~/Movies/Plex/Movies/
+checkpathmusic=~/Movies/Plex/Music/
+checkpathtv_shows=~/Movies/Plex/Tv_Shows/
+checkpathuploaded=~/Movies/Plex/Uploaded/
+checkpathworkout=~/Movies/Plex/Workout/
 
 
 
@@ -22,7 +22,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathplex exists." 
     else
         echo "Error: Directory $checkpathplex does not exist."
-        mkdir $checkpathplex
+        mkdir -p $checkpathplex
         echo " - $checkpathplex has been created"
     fi
 
@@ -32,7 +32,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathdownloaded exists." 
     else
         echo "Error: Directory $checkpathdownloaded does not exist."
-        mkdir $checkpathdownloaded
+        mkdir -p $checkpathdownloaded
         echo " - $checkpathdownloaded has been created"
     fi
 
@@ -42,7 +42,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathlists exists." 
     else
         echo "Error: Directory $checkpathlists does not exist."
-        mkdir $checkpathlists
+        mkdir -p $checkpathlists
         echo " - $checkpathlists has been created"
     fi
 
@@ -52,7 +52,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathmovies exists." 
     else
         echo "Error: Directory $checkpathmovies does not exist."
-        mkdir $checkpathmovies
+        mkdir -p $checkpathmovies
         echo " - $checkpathmovies has been created"
     fi
 
@@ -62,7 +62,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathmusic exists." 
     else
         echo "Error: Directory $checkpathmusic does not exist."
-        mkdir $checkpathmusic
+        mkdir -p $checkpathmusic
         echo " - $checkpathmusic has been created"
     fi
 
@@ -72,7 +72,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathtv_shows exists." 
     else
         echo "Error: Directory $checkpathtv_shows does not exist."
-        mkdir $checkpathtv_shows
+        mkdir -p $checkpathtv_shows
         echo " - $checkpathtv_shows has been created"
     fi
 
@@ -82,7 +82,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathuploaded exists." 
     else
         echo "Error: Directory $checkpathuploaded does not exist."
-        mkdir $checkpathuploaded
+        mkdir -p $checkpathuploaded
         echo " - $checkpathuploaded has been created"
     fi
 
@@ -92,7 +92,7 @@ setup_plex_dirs (){
         echo "Directory $checkpathworkout exists." 
     else
         echo "Error: Directory $checkpathworkout does not exist."
-        mkdir $checkpathworkout
+        mkdir -p $checkpathworkout
         echo " - $checkpathworkout has been created"
     fi
 
